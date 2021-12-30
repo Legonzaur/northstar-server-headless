fff7e25ca75c377449f99eaed37bffcc31db84ad
Legonzaur <legonzaur@live.fr>
2021-12-30 10:56:00 +0100

    set WINEDEBUG to -all

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ export WINEPREFIX=/Wine
 export LIBGL_ALWAYS_SOFTWARE=1
 export __GLX_VENDOR_LIBRARY_NAME=mesa
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/lvp_icd.x86_64.json
-export WINEDEBUG=fixme+all
+export WINEDEBUG=-all
 
 
 wineboot -i -e && wineserver --wait
